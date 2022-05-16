302c7b9117d0bb2f1fc93a4f44a76b09ad65ec77
KelsiP <83795392+KelsiP@users.noreply.github.com>
2022-05-16 14:40:32 -0400

    Add files via upload

---
+++ BoredomCure/jvbc.js
@@ -0,0 +1,27 @@
+
+<script type="text/javascript">
+    function randomlinks(){
+                var myrandom=Math.round(Math.random()*14)
+                var links=new Array()
+                inks[0]="https://www.mapcrunch.com/"
+                links[1]="http://www.news.com"
+                links[2]="https://www.enchambered.com/puzzles/"
+                links[3]="https://freerice.com/"
+                links[4]="https://whattoeat.info/"
+                links[5]="https://www.geoguessr.com/"
+                links[6]="https://10minutemail.com/"
+                links[7]="https://www.youtube.com/"
+                links[8]="https://www.internetlivestats.com/"
+                links[9]="https://agar.io/"
+                links[10]="https://www.space.com/"
+                links[11]="https://www.reddit.com/"
+                links[12]="https://www.eatthismuch.com/"
+                links[13]="https://www.jigsawplanet.com/"
+                links[14]="https://spaceplace.nasa.gov/menu/play/"
+            
+             
+                window.location=links[myrandom]
+    }
+    
+    
+</script>
